4c9c89477bb24478ba588e317d3a4b933618fbc1
Rishabh Agrahari <rishabh.ag342@gmail.com>
2016-07-02 01:36:49 +0530

    material design form

--- patient/models.py
+++ patient/models.py
@@ -1,14 +1,16 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 class Patients(models.Model):
     
-    firstname = models.CharField(max_length = 100)
-    lastname = models.CharField(max_length = 100)
-    location = models.TextField(max_length = 100)
-    level = models.IntegerField()
+    firstname = models.CharField(max_length = 20)
+    lastname = models.CharField(max_length = 20)
+    location = models.CharField(max_length = 200)
+    level = models.IntegerField(validators=[MinValueValidator(1),
+                                       MaxValueValidator(3)])
     age = models.IntegerField()
-    gender = models.TextField()
-    comment = models.TextField()
+    gender = models.CharField(max_length=6)
+    comment = models.CharField(max_length = 200)
     
     def __str__(self):
-        return  '%s %s' % (self.firstname, self.location)
+        return  'name : %s %s | location : %s | level : %d | Age : %d | Gender : %s | Comment : %s' % (self.firstname,self.lastname, self.location,
+        	self.level, self.age, self.gender,self.comment)
